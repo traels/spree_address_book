bb93aab7b5c460a995c3a62dcce743fe3d0dcc69
Daniel Dixon <code@danieldixon.com>
2013-11-10 22:09:07 -0600

    Updated routes to work with latest 2-1-stable branch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Spree::Core::Engine.routes.prepend do
+routes = lambda do
+  # Add your extension routes here
   resources :addresses
+end
+
+
+if Spree::Core::Engine.respond_to?(:add_routes)
+  Spree::Core::Engine.add_routes(&routes)
+else
+  Spree::Core::Engine.routes.prepend(&routes)
 end
