3c7cd4c5f9717ca5845498622a67313099a2b7bd
James Lee <james@jameslee.ca>
2013-11-11 11:24:48 -0700

    address_spec passing

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,7 +6,7 @@ describe Spree::Address do
   describe 'spree_address_book address_decorator' do
     let(:address) { FactoryGirl.create(:address) }
     let(:address2) { FactoryGirl.create(:address) }
-    let(:order) { FactoryGirl.create(:order) }
+    let(:order) { FactoryGirl.create(:completed_order_with_totals) }
     let(:user) { FactoryGirl.create(:user) }
 
     before {
@@ -15,7 +15,7 @@ describe Spree::Address do
     }
 
     it 'has required attributes' do
-      Spree::Address.required_fields.should eq([:firstname, :lastname, :address1, :city, :zipcode, :country, :phone])
+      Spree::Address.required_fields.should eq([:firstname, :lastname, :address1, :city, :country])
     end
 
     it 'is editable' do
@@ -26,7 +26,7 @@ describe Spree::Address do
       address.should be_can_be_deleted
     end
 
-    it "isn't editable when there is an associated order" do
+    it "isn't editable when there is an associated completed order" do
       address2.should_not be_editable
     end
 
